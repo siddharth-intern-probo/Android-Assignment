5a451910271a578c1777282433529512c03c25c1
Siddharth Bihani <121893109+siddharth-intern-probo@users.noreply.github.com>
2023-01-10 11:44:23 +0530

    Removed first SplashScreen

--- app/src/main/java/com/probo/proboassignment1/activities/SplashActivity.kt
+++ app/src/main/java/com/probo/proboassignment1/activities/SplashActivity.kt
@@ -5,17 +5,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.probo.proboassignment1.databinding.ActivitySplashBinding
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity: AppCompatActivity() {
 
-    private lateinit var binding: ActivitySplashBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-        binding = ActivitySplashBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        splashScreen.setKeepOnScreenCondition{true}
 
         @Suppress("DEPRECATION")
         Handler().postDelayed({
